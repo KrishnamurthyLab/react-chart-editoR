53208aa65b7a177cc36d7b014b89fafcfbf6ed7b
Bryce Carson <bcars268@mtroyal.ca>
2024-06-28 15:16:03 -0600

    🚧 Convert the package from webpack to jsdelivr
    Use the jsdelivr CDN to fetch the JavaScript module, rather than building it
    locally and shipping it with the package.

--- R/plotly_editor.R
+++ R/plotly_editor.R
@@ -1,34 +1,68 @@
-#' @title Plotly chart editor input control
-#'
-#' @description Create a Plotly chart editor input control.
-#'
+#' @title React Chart Editor
+#' @description Create a Plotly react-chart-editor component UI element. Running
+#'   this function makes your application take a dependency on the
+#'   react-chart-editor React component, retrieved through the jsdelivr CDN as a
+#'   side effect. It uses reactR to create a Shiny Input from the component,
+#'   returning the HTML that can be included anywhere in your application's UI
+#'   code.
+#' @param id The HTML element id of the react-chart-editor to create.
+#' @param editButtonEventName The name of the input event that will be available
+#'   under input$editButtonEventName, for example.
 #' @importFrom reactR createReactShinyInput
 #' @importFrom htmltools htmlDependency tags
-#'
-
-#' @title plotly_editor
 #' @export
-plotly_editor <- function(inputId) {
+reactChartEditor <- function(id, editButtonEventName) {
+  .NotYetUsed(editButtonEventName)
+  sourceURL <- "https://cdn.jsdelivr.net/npm/react-chart-editor@0.46.1/lib/"
+  css <- r"(*.react-chart-editor ~ div.fold { box-sizing: content-box; })"
   reactR::createReactShinyInput(
-    inputId,
-    "plotly_editor",
-    htmltools::htmlDependency(
-      name = "plotly_editor-input",
-      version = "1.0.0",
-      src = "www/reactcharteditor/plotly_editor",
-      package = "reactcharteditor",
-      script = "plotly_editor.js"
-    ),
+    inputId = id,
+    class = "react-chart-editor",
+    dependencies =
+      htmltools::htmlDependency(
+                   name = "react-chart-editor",
+                   version = "0.46.1",
+                   src = sourceURL,
+                   script = list(src = "index.js", integrity = "hash"),
+                   stylesheet = "react-chart-editor.css"
+                 ),
+    ## MAYBE TODO: use a default data set with a default layout.
     default = list(data = list(), layout = list()),
+    ## MAYBE TODO: use a default configuration.
     configuration = list(),
-    htmltools::tags$div
+    container = function(id, class) {
+      htmltools::tags$div(inputId = id, class = class, style = css)
+    }
   )
 }
 
-#' @title update_plotly_editor
+## MAYBE DONT: it might be better to simply do this directly in JavaScript. It
+## doesn't make sense to do pass information like this from JS to R right back
+## to JS.
+#' @title Message the React Chart Editor
+#' @description Message the editor with a list of a value and a configuration.
+#' @param session The session object; the current reactive domain.
+#' @param inputId The HTML element id of the react-chart-editor to update.
+#' @param value The value to pass to the editor through a message.
+#' @param configuration The configuration to pass to the editor through a
+#'   message.
 #' @export
-update_plotly_editor <- function(session, inputId, value, configuration = NULL) {
-  message <- list(value = value)
-  if (!is.null(configuration)) message$configuration <- configuration
-  session$sendInputMessage(inputId, message);
+messageReactChartEditor <-
+  function(session, inputId, value, configuration)
+    session$sendInputMessage(inputId,
+                             list(value = value,
+                                  if (!is.null(configuration))
+                                    configuration = configuration))
+
+## TODO: implement the functionality for this, so that consuming Shiny
+## applications don't need to do anything but load the package and specify where
+## they'd like their chart editor to live in their application.
+#' @title ggplotly
+#' @description A wrapper for ggplotly which specifies the default
+#'   react-chart-editor ui component to target, and includes the edit button in
+#'   the Plotly modebar.
+#' @param ... Aguments to pass on to plotly::ggplotly
+#' @export
+ggplotly <- function(...) {
+  plotly::ggplotly(...)
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# react-chart-editor-R
+# react-chart-editoR
+A small, experimental wrapper 🐜🚧📦 around the React component
+react-chart-editor from Plotly for use in Shiny applications.
